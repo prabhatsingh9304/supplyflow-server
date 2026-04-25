a4d4d137cbc7236cc8fef2bb1742e01c901ea34c
prabhatsingh9304 <exploitdev0@gmail.com>
2026-04-25 18:12:39 +0530

    fix cors error

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -20,15 +19,9 @@ app = FastAPI(
     version="0.1.0",
 )
 
-origins = os.getenv(
-    "CORS_ORIGINS",
-    "http://localhost:5173,http://localhost:3000,https://supplyflow.waspx.ai",
-).split(",")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
